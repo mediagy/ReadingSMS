569d7d016df3df206d55ec0fc346c67740c3ee05
mediagy <267472544@qq.com>
2023-04-28 23:16:21 +0800

    动态申请读取短信的权限

--- app/src/main/java/com/example/readingsms/MainActivity.java
+++ app/src/main/java/com/example/readingsms/MainActivity.java
@@ -1,23 +1,51 @@
 package com.example.readingsms;
 
 import android.content.ContentResolver;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Telephony;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "abc";
+    private static final int PERMISSION_REQUEST_CODE = 100;
     private Button btn_readsms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {// android 6.0之后
+            //如果未获得读取短信的权限
+            if (this.checkSelfPermission("android.permission.READ_SMS") != PackageManager.PERMISSION_GRANTED)
+                //请求读取短信的权限
+                this.requestPermissions(new String[]{"android.permission.READ_SMS"}, PERMISSION_REQUEST_CODE);
+        }
         initView();
+
+    }
+
+    //判断权限申请的结果
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (PERMISSION_REQUEST_CODE == (requestCode)) {//判断请求码
+            for (int i = 0; i < grantResults.length; i++) {//逐一判断请求结果
+                if (PackageManager.PERMISSION_GRANTED != grantResults[i]) {//如果未申请到这一权限，则弹窗提示并跳出循环
+                    Toast.makeText(this, "没有获得权限，无法继续！", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+            }
+        }
     }
 
     private void readSMS() {
