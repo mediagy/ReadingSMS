c13359694046d3805bbf22b451896bfffb0a595d
mediagy <267472544@qq.com>
2023-04-29 00:06:06 +0800

    用ListView显示短信内容

--- app/src/main/java/com/example/readingsms/MainActivity.java
+++ app/src/main/java/com/example/readingsms/MainActivity.java
@@ -9,6 +9,8 @@ import android.provider.Telephony;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -18,6 +20,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final String TAG = "abc";
     private static final int PERMISSION_REQUEST_CODE = 100;
     private Button btn_readsms;
+    private ListView lv_sms;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,25 +60,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //参数3：选择条件，用来选择满足条件的行
         //参数4：条件中的参数
         //参数5：排序
-        Cursor cursor = resolver.query(Telephony.Sms.CONTENT_URI,
+        cursor = resolver.query(Telephony.Sms.CONTENT_URI,
                 new String[]{Telephony.Sms._ID, Telephony.Sms.PERSON, Telephony.Sms.DATE, Telephony.Sms.ADDRESS, Telephony.Sms.BODY},
                 null,
                 null,
-                Telephony.Sms.DEFAULT_SORT_ORDER);
-        while (cursor.moveToNext()) {//遍历查询结果
-            String _id = cursor.getInt(0) + "";
-            String person = cursor.getString(1);
-            String date = cursor.getString(2);
-            String address = cursor.getString(3);
-            String body = cursor.getInt(4) + "";
-            Log.i(TAG, "readSMS: _id:" + _id + " date:" + date + " address:" + address + " person:" + person + " body:" + body);
-        }
-        cursor.close();
+                "date ASC");//改为按时间升序排列
+        //创建一个简单光标适配器adapter
+        //参数1：上下文
+        //参数2：子布局文件id
+        //参数3：光标对象
+        //参数4：查询出来的列
+        //参数5：子布局文件中用于显示各列的控件id，需与参数4逐一对应
+        //参数6：标志位
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                R.layout.listview_item,
+                cursor,
+                new String[]{Telephony.Sms._ID, Telephony.Sms.PERSON, Telephony.Sms.DATE, Telephony.Sms.ADDRESS, Telephony.Sms.BODY},
+                new int[]{R.id.tv_id, R.id.tv_person, R.id.tv_date, R.id.tv_address, R.id.tv_body},
+                0);
+        lv_sms.setAdapter(adapter);//为lv_sms设置适配器
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cursor.close();//关闭光标
     }
 
     private void initView() {
         btn_readsms = (Button) findViewById(R.id.btn_readsms);
         btn_readsms.setOnClickListener(this);
+        lv_sms = findViewById(R.id.lv_sms);
     }
 
     @Override
